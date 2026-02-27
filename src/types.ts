17f1d22f9a35574a1ace15bf1718de1f6936c868
agunga4-collab <agunga4@lazuardi.sch.id>
2026-02-28 04:42:30 +0700

    Add SteamProject and UserInput interfaces

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+
+export interface SteamProject {
+  title: string;
+  compassionPhase: string;
+  materials: string[];
+  science: string;
+  technology: string;
+  engineering: string;
+  arts: string;
+  math: string;
+  assessment: string;
+}
+
+export interface UserInput {
+  topic: string;
+  gradeLevel: string;
+  communityContext: string;
+}
